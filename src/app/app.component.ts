2c9d61366553262bdb47a61b25fc8141ab6a41c6
Kalidas Sake <kalidasake07.gmail.com>
2021-12-25 18:44:02 +0530

    added angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'angular-routing-poc';
+
+  activeLinkIndex = -1;
+  navLinks: { label: string; link: string; index: number; }[];
+
+  constructor(private router: Router) {
+    this.navLinks = [
+      {
+        label: 'Home',
+        link: './',
+        index: 0
+      }, {
+        label: 'Users',
+        link: './users',
+        index: 1
+      }, {
+        label: 'Servers',
+        link: './servers',
+        index: 2
+      },
+    ];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbar, MatToolbarModule } from '@angular/material/toolbar';
 
-import { Routes,RouterModule } from '@angular/router';
+
+
+// import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
@@ -27,9 +33,13 @@ import { ErrorComponent } from './error/error.component';
   ],
   imports: [
     BrowserModule,
-   // RouterModule.forRoot(appRoutes),
-    AppRoutingModule
-    ],
+    // RouterModule.forRoot(appRoutes),
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatIconModule,
+    MatToolbarModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
